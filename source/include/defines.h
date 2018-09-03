dc13cc61dcc74328f94594cb25ac3dc1d3bc97b0
Renato Avellar Nobre <rekanobre@gmail.com>
2018-09-03 11:32:44 -0300

    Explaining merge conflicts

--- source/include/defines.h
+++ source/include/defines.h
@@ -1,21 +1,21 @@
 
 //mr. roboto
 
-#define MOTOR_LEFT OUT_A 
-#define MOTOR_RIGHT OUT_B 
+#define MOTOR_LEFT OUT_A
+#define MOTOR_RIGHT OUT_B
 #define GARRA OUT_C
 #define COLOR_RIGHT IN_1
-#define COLOR_LEFT IN_2
+#define COLOR_LEFT 4 // IN_2
 #define GIRO IN_3
 
 //bernard
 
-#define PORTA OUT_A 
-#define TRAVA OUT_B 
+#define PORTA OUT_A
+#define TRAVA OUT_B
 #define SIDE_ULTRA_LEFT IN_1
-#define FRONT_ULTRA_RIGHT IN_2 
-#define FRONT_ULTRA_RIGHT IN_3 
-#define SIDE_ULTRA_RIGHT IN_4 
+#define FRONT_ULTRA_RIGHT IN_2
+#define FRONT_ULTRA_RIGHT IN_3
+#define SIDE_ULTRA_RIGHT IN_4
 
 //constantes
 
