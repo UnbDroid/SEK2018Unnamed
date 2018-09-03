ff4fe07b9f6fe2f9d885eb630884c0fb9ddda5ce
Valesca Soares <valescasoares@gmail.com>
2018-09-03 14:24:00 +0000

    atualização dos defines

--- source/include/defines.h
+++ source/include/defines.h
@@ -1,32 +1,23 @@
-#define GREEN_COUNT 300
-#define L_ROTATION_DEGREE 700
-#define R_ROTATION_DEGREE 600
-#define BASE_PWR 70
-#define KP 0.8 //0.5 ok //0.7 melhor
-#define KI 0.0
-#define KD 0.0
-#define RAW_WHITE 456
-#define RAW_BLACK 220
-#define BLACK 1
-#define WHITE 6
-#define MAX_DIST 25
-#define MIN_DIST 20
-#define MIN_RED 125
-#define MAX_RED 175
+
+//mr. roboto
+
+#define MOTOR_LEFT OUT_A 
+#define MOTOR_RIGHT OUT_B 
+#define GARRA OUT_C
+#define COLOR_RIGHT IN_1
 #define COLOR_LEFT IN_2
-#define COLOR_RIGHT IN_3
-#define FRONT_ULTRA IN_4
-#define SIDE_ULTRA IN_1
-#define MOTOR_LEFT OUT_A
-#define MOTOR_RIGHT OUT_C
-#define MOTOR_BOTH OUT_AC
-#define MOTOR_CLAW OUT_B
-#define TURN_LEFT 1
-#define TURN_RIGHT 2
-#define GO_FOWARD 0
-#define GREEN 3
-#define MIN_GREEN_COUNT 10
-#define TRUE 1
-#define FALSE 0
+#define GIRO IN_3
+
+//bernard
+
+#define PORTA OUT_A 
+#define TRAVA OUT_B 
+#define SIDE_ULTRA_LEFT IN_1
+#define FRONT_ULTRA_RIGHT IN_2 
+#define FRONT_ULTRA_RIGHT IN_3 
+#define SIDE_ULTRA_RIGHT IN_4 
+
+//constantes
+
 
 void playTone();
