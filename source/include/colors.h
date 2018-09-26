68b5bd5d7d62c73199eb23da770e9731cfb634e5
gabrielgcp <gabriel.gcp1@gmail.com>
2018-09-26 12:37:47 +0000

    Alteracao dos codigos das cores, obtendo os valores em todas as faixas

--- source/include/colors.h
+++ source/include/colors.h
@@ -7,14 +7,38 @@
   Versao: 1.0
 */
 
-#define AMARELO 0
-#define VERDE 0
-#define AZUL 0
-#define VERMELHO 0
-#define LARANJA 0
-#define ROXO 0
-#define PRETO 0
-#define BRANCO 0
+#define AMARELO_R 255
+#define AMARELO_G 255
+#define AMARELO_B 42
+#define AMARELO_W 255
+#define VERDE_R 19
+#define VERDE_G 40
+#define VERDE_B 30
+#define VERDE_W 73
+#define AZUL_R 22
+#define AZUL_G 43
+#define AZUL_B 76
+#define AZUL_W 124
+#define VERMELHO_R 140
+#define VERMELHO_G 33
+#define VERMELHO_B 11
+#define VERMELHO_W 135
+#define LARANJA_R 120
+#define LARANJA_G 48
+#define LARANJA_B 20
+#define LARANJA_W 152
+#define ROXO_R 69
+#define ROXO_G 55
+#define ROXO_B 116
+#define ROXO_W 204
+#define PRETO_R 20
+#define PRETO_G 23
+#define PRETO_B 51
+#define PRETO_W 54
+#define BRANCO_R 255
+#define BRANCO_G 255
+#define BRANCO_B 255
+#define BRANCO_W 255
 
 #define ID_AMARELO 0
 #define ID_VERDE 1
@@ -31,7 +55,7 @@
     - color: O valor inteiro identificador da cor
   Retorno: O valor default usado para identificaf a cor
 */
-float getDefaultValue(int color);
+// float getDefaultValue(int color);
 
 /* Get Color Name
   Descricao: Retorna o nome das cores dado um identificador
