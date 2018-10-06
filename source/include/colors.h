1b21d680736e6a0d89619c218618f503b131b8f5
Renato Avellar Nobre <rekanobre@gmail.com>
2018-10-06 17:16:16 -0300

    Leitura de Cores 1.0
    Funciona parcialmente, confunde preto, verde e queda

--- source/include/colors.h
+++ source/include/colors.h
@@ -7,47 +7,43 @@
   Versao: 1.0
 */
 
-#define AMARELO_R 255
-#define AMARELO_G 255
-#define AMARELO_B 42
-#define AMARELO_W 255
-#define VERDE_R 19
-#define VERDE_G 40
-#define VERDE_B 30
-#define VERDE_W 73
-#define AZUL_R 22
-#define AZUL_G 43
-#define AZUL_B 76
-#define AZUL_W 124
-#define VERMELHO_R 140
-#define VERMELHO_G 33
-#define VERMELHO_B 11
-#define VERMELHO_W 135
-#define LARANJA_R 120
-#define LARANJA_G 48
-#define LARANJA_B 20
-#define LARANJA_W 152
-#define ROXO_R 69
-#define ROXO_G 55
-#define ROXO_B 116
-#define ROXO_W 204
-#define PRETO_R 20
-#define PRETO_G 23
-#define PRETO_B 51
-#define PRETO_W 54
-#define BRANCO_R 255
-#define BRANCO_G 255
-#define BRANCO_B 255
-#define BRANCO_W 255
+const int amareloRight[] = {255,244,42,255};
+const int verdeRight[] = {19, 40, 30, 73};
+const int azulRight[] = {22, 43, 76, 124};
+const int vermelhoRight[] = {140, 33, 11, 135};
+const int pretoRight[] = {20, 23, 51, 54};
+const int brancoRight[] = {255, 255, 255, 255};
+
+
+const int amareloLeft[] = {255,244,42,255};
+const int verdeLeft[] = {19, 40, 30, 73};
+const int azulLeft[] = {22, 43, 76, 124};
+const int vermelhoLeft[] = {140, 33, 11, 135};
+const int pretoLeft[] = {20, 23, 51, 54};
+const int brancoLeft[] = {255, 255, 255, 255};
+
+
+const int intervals[] = {15, 30, 45, 60, 75, 90, 105, 120, 135, 150, 165, 180, 195, 210, 225, 240, 255};
+
+int colorsMat[][] = {{0,0,0},
+                    {0,0,0},
+                    {0,0,0},
+                    {0,0,0}};
+
+#define INTERVAL_SIZE 15
+
+#define MEAN_ERROR 8
 
 #define ID_AMARELO 0
 #define ID_VERDE 1
 #define ID_AZUL 2
 #define ID_VERMELHO 3
-#define ID_LARANJA 4
-#define ID_ROXO 5
-#define ID_PRETO 6
-#define ID_BRANCO 7
+#define ID_PRETO 4
+#define ID_BRANCO 5
+#define ID_QUEDA 6
+
+#define SUM 1
+#define SUB 0
 
 /* Get Default Value
   Descricao: Busca os valores defaults para identificar as cores
@@ -64,3 +60,19 @@
   Retorno: O nome da cor
 */
 string getColorName(int color);
+
+int intervalValue(int value);
+int intervalError(int value, int sum);
+void calculateInterval(int r, int g, int b, int w);
+int evalInterval(int value[], int expectedInterval);
+int isItColor(int intervalR, int intervalG, int intervalB, int intervalW);
+
+int leftBasicRules(int r, int g, int b, int w);
+int rightBasicRules(int r, int g, int b, int w);
+
+int leftSensorColorRead();
+int rightSensorColorRead();
+
+
+// void leftSensorBasicRules();
+// void rightSensorBasicRules();
