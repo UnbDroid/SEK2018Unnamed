6f576843c0a4f5892e1463bb7ff7d891c9e1fced
Renato Avellar Nobre <rekanobre@gmail.com>
2018-10-10 10:34:23 -0300

    Modularizando a Main

---
+++ source/include/main.h
@@ -0,0 +1,27 @@
+
+#ifndef MAIN_HEADER
+#define MAIN_HEADER
+
+/* ====== INICIO DAS IMPORTACOES DO ARQUIVO MAIN ====== */
+
+#include "include/defines.h"
+#include "include/config.h"
+
+#include "lib/move.nxc"
+
+#ifndef INCFILESIO
+  #include "lib/filesIO.nxc"
+  #define INCFILESIO
+#endif
+
+#ifndef INCCOL
+  #include "lib/colors.nxc"
+  #define INCCOL
+#endif
+
+/* ====== INICIO DAS FUNCOES DO ARQUIVO MAIN ====== */
+
+void testeArquivo();
+void testeCor();
+
+#endif
